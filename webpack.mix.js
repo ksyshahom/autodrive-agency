b99952bc1059a4774bdd06d8f1cf95efed6d3045
ksyshahom <ksysha.hom@gmail.com>
2021-03-18 22:54:24 +0300

    добавила шрифты.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,9 @@ mix.setPublicPath('dist');
 mix.js('src/js/vue.js', './dist').vue();
 mix.sass('src/scss/main.scss', './dist');
 
+const path = require('path');
+
+mix.options({ processCssUrls: false });
 mix.webpackConfig({
     resolve: {
         // alias: {vue: 'vue/dist/vue.esm.js'}
@@ -13,5 +16,66 @@ mix.webpackConfig({
             'vue$': 'vue/dist/vue.esm.js'
         },
         extensions: ['*', '.js', '.vue', '.json']
+    },
+    output: {
+        // filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        // loaders: [
+        //     {
+        //         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        //         use: [
+        //             {
+        //                 loader: 'file-loader',
+        //                 options: {
+        //                     name: '[name].[ext]',
+        //                     outputPath: 'fonts/'
+        //                 }
+        //             }
+        //         ]
+        //     }
+        // ]
+        // output: {
+        //     // filename: 'bundle.js',
+        //     path: path.resolve(__dirname, 'dist'),
+        // },
+        // rules: [
+        //     {
+        //         test: /\.(ttf|eot|woff|woff2)$/,
+        //         use : {
+        //             loader : "file-loader",
+        //             options: {
+        //                 name: "../fonts/[name].[ext]",
+        //                 // context: path.resolve(__dirname, 'src2/'),
+        //                 // useRelativePaths: true,
+        //                 // publicPath: "./dist/",
+        //                 // outputPath: './fonts23/[name].[ext]',
+        //                 // outputPath: 'fonts2/',
+        //             },
+        //         }
+        //         // test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        //         // use: {
+        //         //     loader: 'file-loader',
+        //         //     options: {
+        //         //         name: '[name].[ext]',
+        //         //         outputPath: 'fonts/'
+        //         //     }
+        //         // }
+        //
+        //     },
+        //     // {
+        //     //     test: /\.(scss)$/,
+        //     //     loaders: [
+        //     //         "style-loader",
+        //     //         {
+        //     //             loader:'css-loader',
+        //     //             options: {
+        //     //                 url: false
+        //     //             }
+        //     //         },
+        //     //         "sass-loader"],
+        //     // }
+        // ]
     }
 });
